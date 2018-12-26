4d7363fdac46408ecaa5acaedeb717da609d5b08
Vadym Milichev <Vadym.Milichev@thomsonreuters.com>
2018-12-27 00:14:21 +0200

    CircularDependencyPlugin added to dev build

--- scripts/config-overrides.dev.js
+++ scripts/config-overrides.dev.js
@@ -1,5 +1,17 @@
 const customizeLoaders = require('./customize-loaders');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
 
 module.exports = function (config) {
   customizeLoaders(config.module.rules);
+  addCircularDependencyPlugin(config.plugins);
 };
+
+function addCircularDependencyPlugin(plugins) {
+  plugins.push(
+    new CircularDependencyPlugin({
+      failOnError: true,
+      exclude: /node_modules/,
+      cwd: process.cwd(),
+    })
+  );
+}
